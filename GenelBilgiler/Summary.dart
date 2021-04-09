a4bd8f2078f3143e5a114c018b5267056ca8eeb6
caglar <eflatun126@gmail.com>
2021-04-10 01:12:40 +0300

    ...

---
+++ GenelBilgiler/Summary.dart
@@ -0,0 +1,8 @@
+
+
+/*
+ Dart dili bence gelecek vaad eden bir dil ve arkasında google gibi bir dev olduğu içinde ileride çok iyi
+ yerlere gelebileceğini düşünüyorum bu sebeple sadece flutter için değil dilinde incelikleri ni yavaştan
+ edinmek yararlı olacaktır*/
+
+
